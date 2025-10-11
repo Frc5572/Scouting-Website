440586fde86e2bcd38158550c4e5ad42d5702f7f
GitHub Action <action@github.com>
2025-10-11 02:45:02 +0000

    Update match data for default event

--- match-data.js
+++ match-data.js
@@ -1,5 +1,5 @@
 // Auto-generated match data from The Blue Alliance API
-// Last updated: 2025-08-26T23:01:57.684Z
+// Last updated: 2025-10-11T02:45:02.103Z
 window.MATCH_DATA = {
   "2025txbel": {
     "1": [
@@ -2623,6 +2623,808 @@ window.MATCH_DATA = {
       {
         "key": "frc5103",
         "number": "5103",
+        "alliance": "red"
+      }
+    ]
+  },
+  "2025txhou2": {
+    "1": [
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "red"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      }
+    ],
+    "2": [
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "red"
+      },
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "red"
+      }
+    ],
+    "3": [
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "red"
+      }
+    ],
+    "4": [
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "red"
+      }
+    ],
+    "5": [
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "red"
+      },
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "red"
+      },
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "red"
+      }
+    ],
+    "6": [
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "red"
+      }
+    ],
+    "7": [
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "red"
+      },
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      }
+    ],
+    "8": [
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "red"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "red"
+      },
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "red"
+      }
+    ],
+    "9": [
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "red"
+      }
+    ],
+    "10": [
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "red"
+      }
+    ],
+    "11": [
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      }
+    ],
+    "12": [
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "red"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "red"
+      }
+    ],
+    "13": [
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "red"
+      }
+    ],
+    "14": [
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      },
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "red"
+      }
+    ],
+    "15": [
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "red"
+      }
+    ],
+    "16": [
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "red"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "red"
+      }
+    ],
+    "17": [
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      }
+    ],
+    "18": [
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      },
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "red"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "red"
+      }
+    ],
+    "19": [
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "red"
+      }
+    ],
+    "20": [
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "red"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "red"
+      }
+    ],
+    "21": [
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "red"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      }
+    ],
+    "22": [
+      {
+        "key": "frc4587",
+        "number": "4587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5572",
+        "number": "5572",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc3735",
+        "number": "3735",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5892",
+        "number": "5892",
+        "alliance": "red"
+      },
+      {
+        "key": "frc9992",
+        "number": "9992",
+        "alliance": "red"
+      }
+    ],
+    "23": [
+      {
+        "key": "frc118",
+        "number": "118",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9478",
+        "number": "9478",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5414",
+        "number": "5414",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "red"
+      },
+      {
+        "key": "frc5829",
+        "number": "5829",
+        "alliance": "red"
+      },
+      {
+        "key": "frc324",
+        "number": "324",
+        "alliance": "red"
+      }
+    ],
+    "24": [
+      {
+        "key": "frc127",
+        "number": "127",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8576",
+        "number": "8576",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9994",
+        "number": "9994",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8515",
+        "number": "8515",
+        "alliance": "red"
+      },
+      {
+        "key": "frc6243",
+        "number": "6243",
+        "alliance": "red"
+      },
+      {
+        "key": "frc3847",
+        "number": "3847",
+        "alliance": "red"
+      }
+    ],
+    "25": [
+      {
+        "key": "frc2587",
+        "number": "2587",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc9995",
+        "number": "9995",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc5427",
+        "number": "5427",
+        "alliance": "blue"
+      },
+      {
+        "key": "frc8370",
+        "number": "8370",
+        "alliance": "red"
+      },
+      {
+        "key": "frc2585",
+        "number": "2585",
+        "alliance": "red"
+      },
+      {
+        "key": "frc118",
+        "number": "118",
         "alliance": "red"
       }
     ]
