91d7b13c10634ace0dc3da821d50df3b4f8dbef5
Cole Roberts <118279645+STEMINREALLIFE@users.noreply.github.com>
2026-07-15 13:33:04 -0500

    Update CACHE_NAME version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rosbots-5572-scouting-v3";
+const CACHE_NAME = "rosbots-5572-scouting-v3.001";
 
 // Everything the app needs to run with ZERO network connection.
 const PRECACHE_URLS = [
